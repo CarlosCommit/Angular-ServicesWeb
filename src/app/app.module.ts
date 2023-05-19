3c54d42432115e47a561be788eee695f06d464f0
Carlos Mamani <carlosfernando21334@gmail.com>
2023-05-19 00:13:26 -0300

    se elimina el punto1 porque no consume servicios es solo un layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { Punto1Component } from './components/punto1/punto1.component';
 import { Punto2Component } from './components/punto2/punto2.component';
 import { Punto3Component } from './components/punto3/punto3.component';
 import { Punto4Component } from './components/punto4/punto4.component';
@@ -18,7 +17,6 @@ import { TextToSpeechComponent } from './components/text-to-speech/text-to-speec
   declarations: [
     AppComponent,
     HeaderComponent,
-    Punto1Component,
     Punto2Component,
     Punto3Component,
     Punto4Component,
